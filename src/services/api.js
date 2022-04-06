68288e041668679571ea09a766b0d9cc23195e64
samle1994 <lesamcntta@gmail.com>
2022-04-06 09:17:21 +0700

    edit

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,22 @@
 import axios from "axios";
 const url= {
-    baseUrl:'',
+    baseUrl:'restfulapi.dnd-group.net/api',
     url:'/login'
 }
+const instance=axios.create({
+    baseUrl:url.baseUrl,
+    headers: {
+        "Content-Type":"application/json",
+        Accept:"application/json"
+    }
+});
 const api ={
-    url
+    url:url,
+    instance:instance,
+    get:instance.get,
+    post:instance.post,
+    put:instance.put,
+    delete:instance.delete,
+    patch:instance.patch,
 }
 export default api;
